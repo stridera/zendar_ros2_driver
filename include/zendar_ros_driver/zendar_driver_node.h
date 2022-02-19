dfdde10b964ae1e2f1fd0cab8e769659555a958d
sherwinlau <42704604+sherwinlau@users.noreply.github.com>
2022-02-18 16:31:14 -0800

    added files, unable to link

---
+++ include/zendar_ros_driver/zendar_driver_node.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/PointCloud2.h>
+
+#include <zendar/api/api.h>
+
+namespace zen {
+class ZendarDriverNode {
+public:
+  ZendarDriverNode(
+    const ros::NodeHandle& node,
+    const std::string& url,
+    int argc,
+    char* argv[]
+  );
+  ~ZendarDriverNode();
+  void Run();
+private:
+  ros::NodeHandle node;
+  std::unique_ptr<image_transport::ImageTransport> image_transport;
+
+  void PublishPointCloud(
+    std::unordered_map<std::string, ros::Publisher>* cloud_publishers,
+    std::unordered_map<std::string, ros::Publisher>* cloud_metadata_publishers);
+  void PublishImage(
+    std::unordered_map<std::string, image_transport::Publisher>* image_publishers);
+  void PublishLogs(ros::Publisher& log_publisher);
+  void PublishPoseQuality(ros::Publisher& pose_quality_publisher);
+  void PublishPose(ros::Publisher& pose_publisher);
+  geometry_msgs::PoseStamped ConvertToPoseStamped(const zpb::tracker::message::TrackerState& cloud_data);
+  sensor_msgs::PointCloud2 ConvertToPointCloud2(const zpb::tracker::message::TrackerState& cloud_data);
+};
+}  // namespace zen
---
+++ src/main.cc
@@ -0,0 +1,16 @@
+#include <glog/logging.h>
+#include <ros/ros.h>
+#include <string>
+#include "zendar_ros_driver/zendar_driver_node.h"
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv, "zendar_driver_node");
+  ros::NodeHandle node("~");
+  std::string url;
+  if (!node.getParam("url", url))
+    LOG(FATAL) << "IP address of ZPU was not provided";
+  zen::ZendarDriverNode converter(node, url, argc, argv);
+  // loop until shut down
+  converter.Run();
+  return 0;
+}
