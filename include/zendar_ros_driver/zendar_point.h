36e33667b11633f71cb7ada90f04608aa957cdaf
sherwin <sherwin@zendar.io>
2022-03-09 15:08:59 -0800

    undo linter problems

--- include/zendar_ros_driver/zendar_point.h
+++ include/zendar_ros_driver/zendar_point.h
@@ -24,9 +24,18 @@ struct ZendarPoint {
 
 POINT_CLOUD_REGISTER_POINT_STRUCT(
     zen::ZendarPoint,
-    (float, x, x)(float, y, y)(float, z, z)(double, x_ecef,
-                                            x_ecef)(double, y_ecef, y_ecef)(
-        double, z_ecef, z_ecef)(float, mag, mag)(float, az_var,
-                                                 az_var)(float, el_var, el_var)(
-        float, r, r)(float, rad_vel, rad_vel)(float, az, az)(float, el, el)(
-        float, doa_snr_db, doa_snr_db)(float, rd_mean_snr_db, rd_mean_snr_db));
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (double, x_ecef, x_ecef)
+    (double, y_ecef, y_ecef)
+    (double, z_ecef, z_ecef)
+    (float, mag, mag)
+    (float, az_var, az_var)
+    (float, el_var, el_var)
+    (float, r, r)
+    (float, rad_vel, rad_vel)
+    (float, az, az)
+    (float, el, el)
+    (float, doa_snr_db, doa_snr_db)
+    (float, rd_mean_snr_db, rd_mean_snr_db));
